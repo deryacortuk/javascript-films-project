912be6fb88b75cef551dde26e6e729c97cd5fe86
deryacortuk <45028347+deryacortuk@users.noreply.github.com>
2019-08-12 23:09:56 +0300

    ui class

---
+++ ui.js
@@ -0,0 +1,53 @@
+class UI {
+    static addFilmToUI(newFilm) {
+        const filmList = document.getElementById("films");
+        filmList.innerHTML += `  
+        <tr>
+        <td><img src="${newFilm.url}" class="img-fluid img-thumbnail"></td>
+            <td>${newFilm.title}</td>
+        <td>${newFilm.director}</td>
+        <td><a href="#" id = "delete-film" class = "btn btn-danger">clear film</a></td>
+        </tr>                                         
+        `;
+    }
+    static clearInputs(element1, element2, element3) {
+        element1.value = "";
+        element2.value = "";
+        element3.value = "";
+    }
+    static displayMessages(message, type) {
+        const cardBody = document.querySelector(".card-body");
+        // alert div created
+        const div = document.createElement("div");
+        div.className = `alert alert-${type}`;
+        div.textContent = message;
+        cardBody.appendChild(div);
+        setTimeout(function() {
+            div.remove();
+        }, 1000);
+
+    }
+    static loadAllFilms(films) {
+        const filmList = document.getElementById("films");
+        films.forEach(function(film) {
+            filmList.innerHTML +=
+                `  <tr>
+        <td><img src="${film.url}" class="img-fluid img-thumbnail"></td>
+            <td>${film.title}</td>
+        <td>${film.director}</td>
+        <td><a href="#" id = "delete-film" class = "btn btn-danger">clear film</a></td>
+        </tr>                                         
+        `;
+
+        });
+    }
+    static deleteFilmUI(element) {
+        element.parentElement.parentElement.remove();
+    }
+    static clearAllFilmsUI() {
+        const filmList = document.getElementById("films");
+        while (filmList.firstElementChild !== null) {
+            filmList.firstElementChild.remove();
+        }
+    }
+}
